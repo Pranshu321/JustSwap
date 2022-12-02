76ead51dea865dfa0e98e8db744679304670e217
Pranshu321 <pranshujain0111@gmail.com>
2022-12-02 17:21:57 +0530

    Justswap stater files

---
+++ components/index.js
@@ -0,0 +1,22 @@
+// import HeroSection from "./HeroSection/HeroSection";
+import Model from "./Modal/Model";
+import NavBar from "./NavBar/NavBar";
+import SearchToken from "./SearchToken/SearchToken";
+import Toggle from "./Toggle/Toggle";
+import TokenList from "./TokenList/TokenList";
+import Token from "./Token/Token";
+import AllTokens from "./AllTokens/AllTokens";
+import PoolAdd from "./PoolAdd/PoolAdd";
+import PoolConnect from "./PoolConnect/PoolConnect";
+
+export {
+  Model,
+  NavBar,
+  SearchToken,
+  Toggle,
+  TokenList,
+  Token,
+  AllTokens,
+  PoolAdd,
+  PoolConnect,
+};
