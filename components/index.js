cd2abfcbd976e52303c6b22c60d2ef44b10dd965
Pranshu321 <pranshujain0111@gmail.com>
2022-12-04 01:06:26 +0530

    Changes for deploy 2

--- components/index.js
+++ components/index.js
@@ -1,14 +1,14 @@
 // import HeroSection from "./HeroSection/HeroSection";
-import Model from "./Modal/Model";
-import NavBar from "./NavBar/NavBar";
-import SearchToken from "./SearchToken/SearchToken";
-import Toggle from "./Toggle/Toggle";
-import TokenList from "./TokenList/TokenList";
-import Token from "./Token/Token";
-import PoolAdd from "./PoolAdd/PoolAdd";
-import AllTokens from "./AllTokens/AllTokens";
-import PoolConnect from "./PoolConnect/PoolConnect";
-import HeroSection from "./Hero/Hero";
+import Model from "./Modal/Model.jsx";
+import NavBar from "./NavBar/NavBar.jsx";
+import SearchToken from "./SearchToken/SearchToken.jsx";
+import Toggle from "./Toggle/Toggle.jsx";
+import TokenList from "./TokenList/TokenList.jsx";
+import Token from "./Token/Token.jsx";
+import PoolAdd from "./PoolAdd/PoolAdd.jsx";
+import AllTokens from "./AllTokens/AllTokens.jsx";
+import PoolConnect from "./PoolConnect/PoolConnect.jsx";
+import HeroSection from "./Hero/Hero.jsx";
 
 export {
   Model,
