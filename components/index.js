4891af6460fed49a76624168afd17c60008c7ef3
Pranshu321 <pranshujain0111@gmail.com>
2022-12-04 00:54:43 +0530

    Changes for deploy

--- components/index.js
+++ components/index.js
@@ -5,8 +5,8 @@ import SearchToken from "./SearchToken/SearchToken";
 import Toggle from "./Toggle/Toggle";
 import TokenList from "./TokenList/TokenList";
 import Token from "./Token/Token";
-import AllTokens from "./AllTokens/AllTokens";
 import PoolAdd from "./PoolAdd/PoolAdd";
+import AllTokens from "./AllTokens/AllTokens";
 import PoolConnect from "./PoolConnect/PoolConnect";
 import HeroSection from "./Hero/Hero";
 
