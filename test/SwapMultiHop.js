cdb09fae9971df974953f740e6a9b052c2552d3e
Pranshu321 <pranshujain0111@gmail.com>
2022-12-06 11:51:15 +0530

    MultiSwap Contract Test and compile

--- test/SwapMultiHop.js
+++ test/SwapMultiHop.js
@@ -5,8 +5,8 @@ const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-describe("SingleSwapToken", () => {
-    let singleSwapToken;
+describe("SwapMultiHop", () => {
+    let swapMultiHop;
     let accounts;
     let weth;
     let dai;
@@ -15,41 +15,40 @@ describe("SingleSwapToken", () => {
     before(async () => {
         accounts = await ethers.getSigners(1);
 
-        const SingleSwapToken = await ethers.getContractFactory("SingleSwapToken");
-        singleSwapToken = await SingleSwapToken.deploy();
-        await singleSwapToken.deployed();
+        const SwapMultiHop = await ethers.getContractFactory("SwapMultiHop");
+        swapMultiHop = await SwapMultiHop.deploy();
+        await swapMultiHop.deployed();
 
         weth = await ethers.getContractAt("IWETH", WETH9);
         dai = await ethers.getContractAt("IERC20", DAI);
         usdc = await ethers.getContractAt("IERC20", USDC);
     });
 
-    it("swapExactInputSingle", async () => {
+    it("swapExactInputMultihop", async () => {
         const amountIn = 10n ** 18n;
 
         // Deposit WETH
         await weth.deposit({ value: amountIn });
-        await weth.approve(singleSwapToken.address, amountIn);
+        await weth.approve(swapMultiHop.address, amountIn);
 
         // Swap
-        await singleSwapToken.swapExactInputSingle(amountIn);
+        await swapMultiHop.swapExactInputMultihop(amountIn);
         console.log("DAI balance", await dai.balanceOf(accounts[0].address));
     });
 
-    it("swapExactOutputSingle", async () => {
+    it("swapExactOutputMultihop", async () => {
         const wethAmountInMax = 10n ** 18n;
         const daiAmountOut = 100n * 10n ** 18n;
 
         //DEPOSIT WETH
         await weth.deposit({ value: wethAmountInMax });
-        await weth.approve(singleSwapToken.address, wethAmountInMax);
+        await weth.approve(swapMultiHop.address, wethAmountInMax);
 
         //SWAP
-        await singleSwapToken.swapExactOutputSingle(daiAmountOut, wethAmountInMax);
-        // console.log(accounts);
-        // console.log(accounts[0].address);
+        await swapMultiHop.swapExactOutputMultihop(daiAmountOut, wethAmountInMax);
+        console.log(accounts[0].address);
         // console.log(accounts[1].address);
         console.log("Dai balance", await dai.balanceOf(accounts[0].address));
-        console.log("Dai balance", await dai.balanceOf(accounts[1].address));
+        // console.log("Dai balance", await dai.balanceOf(accounts[1].address));
     });
 });
