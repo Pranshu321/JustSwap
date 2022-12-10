27e03bf1b5ed625460713c9439fdb32939f596b7
Pranshu321 <pranshujain0111@gmail.com>
2022-12-10 13:17:08 +0530

    Tokens page

--- Context/constants.js
+++ Context/constants.js
@@ -9,16 +9,16 @@ import singleSwapToken from './SingleSwapToken.json';
 import swapMultiHop from './SwapMultiHop.json';
 import IWETH from './IWETH.json';
 
-export const BooTokenAddress = "0xc6B407503dE64956Ad3cF5Ab112cA4f56AA13517";
+export const BooTokenAddress = "0xc6b407503de64956ad3cf5ab112ca4f56aa13517";
 export const BooTokenABI = booToken.abi;
 
-export const LifeTokenAddress = "0x3a622DB2db50f463dF562Dc5F341545A64C580fc";
+export const LifeTokenAddress = "0x3a622db2db50f463df562dc5f341545a64c580fc";
 export const LifeTokenABI = lifeToken.abi;
 
-export const SingleSwapTokenAddress = "0x6A47346e722937B60Df7a1149168c0E76DD6520f";
+export const SingleSwapTokenAddress = "0x6a47346e722937b60df7a1149168c0e76dd6520f";
 export const SingleSwapTokenABI = singleSwapToken.abi;
 
-export const SwapMultiHopAddress = "0x7A28cf37763279F774916b85b5ef8b64AB421f79";
+export const SwapMultiHopAddress = "0x7a28cf37763279f774916b85b5ef8b64ab421f79";
 export const SwapMultiHopABI = swapMultiHop.abi;
 
 export const IWETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
